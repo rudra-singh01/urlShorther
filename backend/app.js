cbdd9dc9f2ad12f6f237b950c6953ca3dbbde527
rudra-singh01 <rudrasingh812006@gmail.com>
2025-07-17 14:13:33 +0530

    change the cors

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://url-shorther-zjxm.vercel.app/",
   credentials: true,
 }));
 app.use(attchUser)
